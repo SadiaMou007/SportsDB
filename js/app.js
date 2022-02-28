3a53f6f5503b40884653f8540c04401d3a56c96f
Your Name <you@example.com>
2022-02-28 04:10:34 -0800

    add search result

---
+++ js/app.js
@@ -0,0 +1,35 @@
+const srcField = document.getElementById("src-field");
+const dataContainer = document.getElementById("dataContainer");
+// 1. fetch api by name
+const loadApi = () => {
+  const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${srcField.value}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showResult(data.player));
+};
+// 2. show results
+const showResult = (data) => {
+  data.forEach((element) => {
+    console.log(element);
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div class="col mb-2 mcard">
+    <div class="bg-light h-100 cursor d-flex justify-content-center align-items-center flex-column rounded">
+    <div class="w-75"><img src="${element.strThumb}" class="card-img-top p-3 mcard-img" alt="...">
+    </div>
+      <div class="card-body">
+        <h5 class="card-title">Name: ${element.strPlayer}</h5>
+        <h5 class="card-title">Country: ${element.strNationality}</h5>
+        <div class="w-100 d-flex">
+        <button type="button" class="btn btn-outline-info w-50 me-1">Details</button>
+        <button type="button" class="btn btn-outline-danger w-50">Delete</button>
+        </div>
+      
+
+      </div>
+    </div>
+    </div>
+    `;
+    dataContainer.appendChild(div);
+  });
+};
